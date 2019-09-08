92ad5cb2a2cd1ddd47e0dfa9fbf32d22a6dad53b
Artturi Hämäläinen <artturi_hamalainen@hotmail.com>
2019-09-08 20:57:46 +0300

    react router dom added

---
+++ src/components/Edit/edit-preview.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const EditPreview = props => {
+  return (
+    <div className="edit-input">
+      <div className="preview "></div>
+    </div>
+  );
+};
+
+export default EditPreview;
